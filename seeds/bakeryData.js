64286b96b7809c1edda0a9b73fdca7b9f22e8a78
PennyLIU2022 <114879207+PennyLIU2022@users.noreply.github.com>
2023-01-26 20:36:54 -0500

    update database and models, add images

--- seeds/bakeryData.js
+++ seeds/bakeryData.js
@@ -3,11 +3,13 @@ const { bakery } = require('../models');
 const bakeryData = [
     {
         bakeryName: "Bagel",
-        bakeryNote: "New York-style bagel topped with garlic, salt, poppy and sesame seeds."
+        bakeryNote: "New York-style bagel topped with garlic, salt, poppy and sesame seeds.",
+        fileName: "01-bagel"
     },
     {
         bakeryName: "Chocolate Chip Cookie",
-        bakeryNote: "Semisweet chocolate chunks mixed in a thick, chewy cookie--soft on the inside and crunchy on the outside."
+        bakeryNote: "Semisweet chocolate chunks mixed in a thick, chewy cookie--soft on the inside and crunchy on the outside.",
+        fileName: "02-chocolate-cookie"
     },
 ];
 
